3d04ff4de9514840bcc627c62f21f18d1cf8edae
Эмир Матов <@itstep.loc>
2023-10-18 21:48:22 +0600

    happy

---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="background">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.background {
+    background-image: url(../assets/enot.jpeg); ;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+import  HomePage from '@/components/HomePage.vue';
 </script>
 
 <template>
   <div class="home">
+    <HomePage />
     <h1>Home</h1>
   </div>
 </template>
