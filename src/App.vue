35fdd2442f0cdec288884d8f5c54d001965d8900
Эмир Матов <@itstep.loc>
2023-11-03 19:33:04 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from '@/components/layout/NavBar.vue'
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <NavBar />
   <RouterView />
-
+  <Footer />
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        <span>© 2023-forever Freepoisk</span>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+footer {
+    position: absolute;
+    height: 100px;
+    right: 0px;
+    left: 0px;
+    background-color: rgb(10, 10, 10);
+    z-index: 1;
+    text-align: center;
+}
+
+span {
+    font-size: 25pt;
+}
+</style>
